411834e42825b9e0e998226b7743c6be18a526bb
10sajan10 <sajanneupane137@gmail.com>
2024-11-21 14:14:03 -0700

    Container Image Publish

---
+++ Docker/docker_publish.sh
@@ -0,0 +1,11 @@
+# Step 1: Authenticate Docker to AWS ECR
+echo "Authenticating Docker to AWS ECR..."
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 123456789012.dkr.ecr.us-east-1.amazonaws.com
+
+# Step 2: Tag the Docker image
+echo "Tagging Docker image..."
+docker tag myimage:latest 123456789012.dkr.ecr.us-east-1.amazonaws.com/my-repository:latest
+
+# Step 3: Push the Docker image to ECR
+echo "Pushing Docker image to ECR..."
+docker push 123456789012.dkr.ecr.us-east-1.amazonaws.com/my-repository:latest
